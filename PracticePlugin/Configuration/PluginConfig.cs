e6445ec49dcec34d6cee79f6552e05dc183dfa4a
デンパ時計 <jsoldenpa@yahoo.co.jp>
2022-05-28 09:37:56 +0900

    使いそうにない設定削除 動いてなかったフルエネルギーを修正

--- PracticePlugin/Configuration/PluginConfig.cs
+++ PracticePlugin/Configuration/PluginConfig.cs
@@ -9,7 +9,6 @@ namespace PracticePlugin.Configuration
         public static PluginConfig Instance { get; set; }
         public virtual bool StartWithFullEnergy { get; set; } = false;
         public virtual bool ShowTimeFailed { get; set; } = true;
-        public virtual bool DisablePitchCorrection { get; set; } = false;
         public virtual bool AdjustNJSWithSpeed { get; set; } = false;
     }
 }
--- PracticePlugin/Models/UIElementsCreator.cs
+++ PracticePlugin/Models/UIElementsCreator.cs
@@ -1,45 +1,38 @@
 ﻿using BeatSaberMarkupLanguage;
 using PracticePlugin.Configuration;
 using PracticePlugin.Views;
+using SiraUtil.Affinity;
 using System.Reflection;
 using UnityEngine;
 using Zenject;
 
 namespace PracticePlugin.Models
 {
-    public class UIElementsCreator : IInitializable
+    public class UIElementsCreator : IInitializable, IAffinity
     {
         private readonly SongSeeker _songSeeker;
-        private readonly GameEnergyCounter _gameEnergyCounter;
         private readonly SongTimeInfoEntity _songTimeInfoEntity;
         private readonly PracticeUI _practiceUI;
         [Inject]
         public UIElementsCreator(
             SongTimeInfoEntity songTimeInfoEntity,
             SongSeeker songSeeker,
-            PracticeUI practiceUI,
-            GameEnergyCounter gameEnergyCounter)
+            PracticeUI practiceUI)
         {
             this._songTimeInfoEntity = songTimeInfoEntity;
             this._songSeeker = songSeeker;
             this._practiceUI = practiceUI;
-            this._gameEnergyCounter = gameEnergyCounter;
         }
         public void Initialize()
         {
             if (!this._songTimeInfoEntity.PracticeMode) {
                 return;
             }
-            if (PluginConfig.Instance.StartWithFullEnergy) {
-                this._gameEnergyCounter.ProcessEnergyChange(1 - this._gameEnergyCounter.energy);
-            }
             var canvas = GameObject.Find("PauseMenu").transform.Find("Wrapper").transform.Find("MenuWrapper").transform.Find("Canvas");
-
             if (canvas == null) {
                 return;
             }
             var uiObj = new GameObject("PracticePlugin Seeker UI", typeof(RectTransform));
-
             (uiObj.transform as RectTransform).anchorMin = new Vector2(0, 0);
             (uiObj.transform as RectTransform).anchorMax = new Vector2(1, 1);
             (uiObj.transform as RectTransform).sizeDelta = new Vector2(0, 0);
@@ -48,6 +41,18 @@ namespace PracticePlugin.Models
             uiObj.transform.localScale = new Vector3(1, 1, 1);
             uiObj.transform.localPosition = new Vector3(0f, -3f, 0f);
             this._songSeeker.gameObject.transform.SetParent(canvas, false);
+        }
+
+        [AffinityPatch(typeof(GameEnergyCounter), nameof(GameEnergyCounter.Start))]
+        [AffinityPostfix]
+        protected void EnergyCounterStartPostfix(GameEnergyCounter __instance)
+        {
+            if (!this._songTimeInfoEntity.PracticeMode) {
+                return;
+            }
+            if (__instance.energyType != GameplayModifiers.EnergyType.Battery && PluginConfig.Instance.StartWithFullEnergy) {
+                __instance.ProcessEnergyChange(1 - __instance.energy);
+            }
         }
     }
 }
